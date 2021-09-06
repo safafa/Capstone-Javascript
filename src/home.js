7236aa130b670368df65cb5e65a898c8d1b13563
safafa <safaaballagh97@gmail.com>
2021-09-06 23:20:29 +0100

    Add button

--- src/home.js
+++ src/home.js
@@ -5,17 +5,21 @@ const displayDish = (dish) => {
     const likeB = document.createElement('button');
     const nameLike = document.createElement('div');
     const button = document.createElement('button');
+    const reservation = document.createElement('button');
     image.setAttribute('src',dish.strMealThumb);
     name.innerText = dish.strMeal;
     likeB.setAttribute('class', 'far fa-heart like');
     button.innerText = 'Comments';
+    reservation.innerText = 'Reservations';
     button.setAttribute('class', 'comments-button');
+    reservation.setAttribute('class', 'comments-button');
     card.appendChild(image);
     nameLike.appendChild(name);
     nameLike.appendChild(likeB);
     nameLike.setAttribute('class', 'name-like flex');
     card.appendChild(nameLike);
     card.appendChild(button);
+    card.appendChild(reservation);
     card.setAttribute('class','card flex');
     return card;
 }
