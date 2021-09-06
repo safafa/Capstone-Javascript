e83cb3a054c3365095e2220104f8b55078506926
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2021-09-06 17:07:42 +0300

    webpack and jest configuration

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development', 
+  entry: './src/index.js',
+  devServer: {
+    static: './dist',
+    port: '8081', 
+  },
+  plugins: [
+   new HtmlWebpackPlugin({
+    title: 'Output Management',
+    template: './src/index.html'
+  }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+    module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
