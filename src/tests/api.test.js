fc66ccb542826714ccf60c479c27477f7bb7beda
safafa <safaaballagh97@gmail.com>
2021-09-08 12:11:37 +0100

    Add unit tests

---
+++ src/tests/api.test.js
@@ -0,0 +1,46 @@
+import { apiGet } from '../api.js';
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({ "meals": [{
+        "strMeal": "Baingan Bharta",
+        "strMealThumb": "https://www.themealdb.com/images/media/meals/urtpqw1487341253.jpg",
+        "idMeal": "52807"
+    },
+    {
+        "strMeal": "Chicken Handi",
+        "strMealThumb": "https://www.themealdb.com/images/media/meals/wyxwsp1486979827.jpg",
+        "idMeal": "52795"
+    },
+    {
+        "strMeal": "Dal fry",
+        "strMealThumb": "https://www.themealdb.com/images/media/meals/wuxrtu1483564410.jpg",
+        "idMeal": "52785"
+    },
+    ] }),
+  })
+);
+
+global.dishes = [{
+    "strMeal": "Baingan Bharta",
+    "strMealThumb": "https://www.themealdb.com/images/media/meals/urtpqw1487341253.jpg",
+    "idMeal": "52807"
+},
+{
+    "strMeal": "Chicken Handi",
+    "strMealThumb": "https://www.themealdb.com/images/media/meals/wyxwsp1486979827.jpg",
+    "idMeal": "52795"
+},
+{
+    "strMeal": "Dal fry",
+    "strMealThumb": "https://www.themealdb.com/images/media/meals/wuxrtu1483564410.jpg",
+    "idMeal": "52785"
+},
+]
+
+describe('apiGet Method', () => {
+    test('it returns the correct number of dishes', () => {
+        expect(apiGet('https://www.themealdb.com/api/json/v1/1/filter.php')).resolves.toBe(dishes);
+        expect(fetch).toHaveBeenCalledTimes(1);
+      })
+})
