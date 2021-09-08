ab59e693b5196bae335eb29c6dbc54937f6f5133
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2021-09-08 18:18:23 +0300

    post and display comments

--- src/comment.js
+++ src/comment.js
@@ -1,7 +1,29 @@
-import { apiGet } from './api.js';
+import { apiGet, apiPost } from './api.js';
 
-const mealRecipeModal = (meal) => {
+const displayComments = (commentDiv, comments) => {
+  if (comments.length >= 1) {
+    commentDiv.innerHTML = '';
+    comments.forEach((element) => {
+      const comP = document.createElement('p');
+      comP.innerHTML = `${element.creation_date} ${element.username}: ${element.comment}`;
+      commentDiv.appendChild(comP);
+    });
+  }
+};
+
+const postComment = async (commentDiv, comment) => {
+  apiPost('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/5UpYqnub5KIZMG9nlN2D/comments', comment)
+    .then(() => {
+      apiGet(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/5UpYqnub5KIZMG9nlN2D/comments?item_id=${comment.item_id}`).then((comments) => { displayComments(commentDiv, comments); });
+    });
+};
+
+const mealRecipeModal = (meal, comments) => {
   const popSec = document.querySelector('.popUp');
+  const userId = meal.idMeal + 1;
+  const commentId = meal.idMeal + 2;
+  const formId = meal.idMeal + 3;
+
   const html = `
     <i class="far fa-window-close" id = "closeBtn"></i>
         <img src="${meal.strMealThumb}" class = "imgPop"alt="">
@@ -10,14 +32,13 @@ const mealRecipeModal = (meal) => {
         <div class="d-flex justify-content-around"><span>video link:<a href="${meal.strYoutube}" >video</a> </span>
         <span>tags: ${meal.strTags}</span></div>
         <h3>Comments(2)</h3>
-        <p> Date name: comments</p>
-        <p>Date name: comments</p>
-        <form>
+       <div id = ${meal.idMeal}></div>
+        <form id = ${formId}>
           <div class="form-group mt-2">
-          <input type="text" class="form-sm-control">
+          <input type="text" class="form-sm-control" id = ${userId} required>
           </div>
           <div class="form-group mt-3">
-          <input type="text" class="form-sm-control">
+          <input type="text" class="form-sm-control" id = ${commentId} required>
           </div>
           <button class="btn btn-dark mt-2">comment</button>
         </form>
@@ -28,10 +49,26 @@ const mealRecipeModal = (meal) => {
   closeBtn.addEventListener('click', () => {
     popSec.style.display = 'none';
   });
+  const commentDiv = document.getElementById(meal.idMeal);
+  displayComments(commentDiv, comments);
+  const form = document.getElementById(formId);
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const userName = document.getElementById(userId);
+    const userComm = document.getElementById(commentId);
+    postComment(commentDiv, {
+      item_id: meal.idMeal,
+      username: userName.value,
+      comment: userComm.value,
+    });
+    form.reset();
+  });
 };
 export const getMealRecipe = (idMeal) => {
   apiGet(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
-    .then((data) => { mealRecipeModal(data.meals[0]); });
+    .then((data) => {
+      apiGet(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/5UpYqnub5KIZMG9nlN2D/comments?item_id=${idMeal}`).then((comments) => { mealRecipeModal(data.meals[0], comments); });
+    });
 };
 
 export default mealRecipeModal;
